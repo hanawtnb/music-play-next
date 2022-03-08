9328e51fea223a95cda5fc811b109bbd36b03301
hanawtnb <hanawtnb@icloud.com>
2022-03-08 13:38:44 +0900

    検索機能を実装

--- store/search.tsx
+++ store/search.tsx
@@ -1,9 +1,27 @@
+import { useRouter } from "next/router";
 import React, { useEffect, useState, createContext, useContext } from "react";
+
+import { Context as TokenContext } from "./token";
 
 export const Context = createContext();
 
 export const Store = ({ children }) => {
-  const [searched, setSearched] = useState([]);
+  const router = useRouter();
+
+  const { token } = useContext(TokenContext);
+
+  const [searched, setSearched] = useState(null);
+
+  const handleSearch = (search) => {
+    fetch(`/api/spotify/${token?.access_token}/artist/${search}`)
+      .then((res) => res.json())
+      .then(({ tracks }) => setSearched(tracks))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    if (router?.query?.search) handleSearch(router.query.search);
+  }, [router?.query?.search]);
 
   return <Context.Provider value={{ searched }}>{children}</Context.Provider>;
 };
