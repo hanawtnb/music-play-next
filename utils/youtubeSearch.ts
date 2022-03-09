ce32b0f54bb64fd92a2b8d9b1ad24039b38761f2
hanawtnb <hanawtnb@icloud.com>
2022-03-09 11:26:27 +0900

    検索結果がawaitされていなかったので修正

--- utils/youtubeSearch.ts
+++ utils/youtubeSearch.ts
@@ -1,4 +1,4 @@
-import * as search from "youtube-search";
+import search from "youtube-search";
 import getConfig from "next/config";
 
 const { publicRuntimeConfig: config } = getConfig();
@@ -8,32 +8,41 @@ const options = {
   key: config.api_key,
 };
 
+/**
+ * youtubeでアーティスト名と曲名を検索.
+ * @remarks - ローカルストレージにvideoIdが格納されていたらそこから返す。格納されていなかったら検索して返す。
+ * @param props - `${artists?.[0]?.name} - ${name} song`
+ * @returns - videoId
+ */
 const youtubeSearch = async (props) => {
-  let storage: any = localStorage.getItem("history");
-  if (storage) storage = JSON.parse(storage);
+  const id = await new Promise((res, rej) => {
+    let storage: any = localStorage.getItem("history");
+    if (storage) storage = JSON.parse(storage);
 
-  console.log(storage);
+    if (storage?.find(({ searchStr }) => searchStr === props)) {
+      res(storage?.find(({ searchStr }) => searchStr === props)?.id);
+    }
 
-  if (storage?.find(({ searchStr }) => searchStr === props)) {
-    return storage?.find(({ searchStr }) => searchStr === props)?.id;
-  }
-
-  console.log("not in history");
-
-  return await search(props, options, (err, res) => {
-    console.log(props);
-
-    if (err) console.error(err);
-    localStorage.setItem(
-      "history",
-      JSON.stringify(
-        storage?.length > 0
-          ? [...storage, { ...res[0], searchStr: props }]
-          : [{ ...res[0], searchStr: props }]
-      )
-    );
-    return res[0]?.id;
+    search(props, options, (err, results) => {
+      if (err) {
+        console.error(err);
+        rej();
+      }
+      localStorage.setItem(
+        "history",
+        JSON.stringify(
+          storage?.length > 0
+            ? [...storage, { ...res[0], searchStr: props }]
+            : [{ ...res[0], searchStr: props }]
+        )
+      );
+      console.dir(results);
+      res(results[0]?.id);
+    });
   });
+  console.log("result", id);
+
+  return id;
 };
 
 export default youtubeSearch;
