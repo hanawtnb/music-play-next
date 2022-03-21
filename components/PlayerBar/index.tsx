76376154acf18119d9794c2b06ca387cc5f611c6
hanawtnb <hanawtnb24@gmail.com>
2022-03-21 21:24:23 +0900

    アーティスト名ページに遷移

--- components/PlayerBar/index.tsx
+++ components/PlayerBar/index.tsx
@@ -2,23 +2,42 @@ import React, { useContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { Context as SongContext } from "store/song";
+import { Context as TokenContext } from "store/token";
 import Controls from "components/Controls";
+import Volume from "components/Volume";
 
 import styles from "./styles.module.scss";
-import Volume from "components/Volume";
-import AddCue from "components/Button/AddCue";
 
 const PlayerBar = () => {
-  const { curSong } = useContext(SongContext);
+  const { curSong, setSearched, searched } = useContext(SongContext);
+  const { handleToken } = useContext(TokenContext);
+  const router = useRouter();
+
+  /**
+   * アーティストを検索.
+   * @param name - アーティスト名
+   */
+  const search = (name: string) => {
+    router.push({ pathname: "/", query: { search: name } }, undefined, {
+      shallow: true,
+    });
+  };
+
   return (
     <div className={styles["bar"]}>
       <div className={styles["controls"]}>
         {curSong && (
           <div className={styles["playing"]}>
             <img src={curSong?.img} />
-            <div className={styles["playing__name"]}>
+            <div>
               <p>{curSong?.name}</p>
-              <p>{curSong?.artists?.map(({ name }) => name)}</p>
+              <p>
+                {curSong?.artists?.map(({ name }) => (
+                  <button key={name} type="button" onClick={() => search(name)}>
+                    {name}
+                  </button>
+                ))}
+              </p>
             </div>
           </div>
         )}
